e1181fe6fec75feff814e164cdb8ab7e9dc9ac2c
Benjamin <benjamin.gosset.pro@gmail.com>
2021-01-29 15:01:49 +0100

    updates on readme, functions.php and styles.css

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bgo-starter
+
+WordPress starter theme
+
+## How to use
+- Command : `npm install`
+- Change URL in gulpfile.js
+- Command : `gulp`
--- functions.php
+++ functions.php
@@ -1,12 +1,44 @@
 <?php 
 
-// Add post thumbnails support
+/**
+ * Add post thumbnails support
+ */ 
 add_theme_support( 'post-thumbnails' );
 
-// Add site title in theme header
+/**
+ * Add site title in theme header
+ */ 
 add_theme_support( 'title-tag' );
 
-// Enqueue styles and scripts
+/**
+* Switch default core markup for search form, comment form, and comments
+* to output valid HTML5.
+*/
+add_theme_support( 'html5', array(
+    'search-form',
+    'comment-form',
+    'comment-list',
+    'gallery',
+    'caption',
+    'script',
+    'style',
+) );
+
+/**
+ * Add support for core custom logo.
+ *
+ * @link https://codex.wordpress.org/Theme_Logo
+ */
+add_theme_support( 'custom-logo', array(
+    'height'      => 50,
+    'width'       => 250,
+    'flex-width'  => true,
+    'flex-height' => true,
+) );
+
+/**
+ * Enqueue styles and scripts
+ */ 
 add_action( 'wp_enqueue_scripts', 'bgo_register_assets' );
 
 function bgo_register_assets() {
@@ -16,8 +48,13 @@ function bgo_register_assets() {
     wp_enqueue_script( 'bgo', get_template_directory_uri() . 'dist/js/script.js', array(), '1.0', true);
 }
 
-// Register menus
+/**
+ * Register menus
+ */ 
 register_nav_menus( array(
-	'main' => 'Menu Principal',
-	'footer' => 'Bas de page',
+	'menu-1'      => esc_html__( 'Primary menu', 'bgostarter' ),
+	'topbar-menu' => esc_html__( 'Top bar menu', 'bgostarter' ),
+	'social-menu' => esc_html__( 'Main menu social icons', 'bgostarter' ),
+	'footer-menu' => esc_html__( 'Footer menu', 'bgostarter' ),
+	'footer-social-menu' => esc_html__( 'Footer social icons', 'bgostarter' ),
 ) );
