f8f6653f4199a0aa5464a9b2b7c430221e672eee
Benjamin <benjamin.gosset.pro@gmail.com>
2022-08-26 21:49:22 +0200

    Add content to single.php

--- front-page.php
+++ front-page.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 
 <code>front-page.php</code>
+
+
+    <ul>
+        <?php wp_list_categories( array(
+            'orderby' => 'name'
+        ) ); ?> 
+    </ul>
+
+    <?php
+        the_content();
+    ?>
     
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
 <code>single.php</code>
+
+<?php the_content(); ?>
     
 <?php get_footer(); ?>
